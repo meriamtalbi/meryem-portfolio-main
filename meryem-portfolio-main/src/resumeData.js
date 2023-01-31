6f7849c9a2d34e8c50f522ef66f69ed54dff3a41
meriamtalbi <79254326+meriamtalbi@users.noreply.github.com>
2023-01-31 14:33:34 +0100

    Add files via upload

---
+++ meryem-portfolio-main/src/resumeData.js
@@ -0,0 +1,88 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/meriamtalbi",
+    "name": "meryem talbe",
+    "role": "full stack developer",
+    "linkedinId":"meryem talbe",
+    "skypeid": "meryem talbe",
+    "roleDescription": "I like dabbling in various parts of frontend and backend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://github.com/meriamtalbi",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/meriamtalbi",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://github.com/meriamtalbi",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a  student at The sup-mti . I am a self taught Full Stack Web Developer, currently diving deeper into .net  . I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"maroc",
+    "website":"https://github.com/meriamtalbi",
+    "education":[
+      {
+        "UniversityName":"Ecole Supérieur de Management, Télécommunication et d'Informatique",
+        "specialization":"genie informatique",
+       
+       
+      },
+      {
+        "UniversityName":"ista",
+        "specialization":"dev",
+        
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"matu",
+        "specialization":"Devops",
+        
+        
+      },
+      {
+        "CompanyName":"Atlas Bottling",
+        "specialization":"Some specialization",
+       
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":".Net"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/app1.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"user666"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"user-25"
+      }
+    ]
+  }
+  
+  export default resumeData
